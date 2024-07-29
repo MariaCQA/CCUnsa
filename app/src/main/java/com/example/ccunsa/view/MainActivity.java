ad2cff147a352e4c182f23209f544ac67733c28a
tty3qp <ycozco@unsa.edu.pe>
2024-07-29 18:12:27 -0500

    nav bar arreglado

--- app/src/main/java/com/example/ccunsa/view/MainActivity.java
+++ app/src/main/java/com/example/ccunsa/view/MainActivity.java
@@ -10,7 +10,6 @@ import com.example.ccunsa.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
-    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +17,14 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Find NavController
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
-        navController = navHostFragment.getNavController();
-
-        // Setup BottomNavigationView with NavController
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
         NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
 
         // Handle FloatingActionButton click
         binding.fab.setOnClickListener(v -> {
-            navController.navigate(R.id.qrFragment);
+            navController.navigate(R.id.QrFragment);
         });
     }
 }
